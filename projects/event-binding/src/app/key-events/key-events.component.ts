1c14c8922690c909c60a17c19047854f54d804a8
Seyed <iammseyed@proton.me>
2024-08-06 18:31:34 +0530

    updated validation part in username

--- projects/event-binding/src/app/key-events/key-events.component.ts
+++ projects/event-binding/src/app/key-events/key-events.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./key-events.component.css']
 })
 export class KeyEventsComponent {
-  public new:any[]=
+  public users:any[]=
   [
     {Username:'un123'},
     {Username:'un234'},
@@ -25,36 +25,44 @@ export class KeyEventsComponent {
   checkUser(e:any){
     let user= e.target.value;
     // alert (user.length)
-    if(user.length>3 && user.length<=8){
-      for(var u of this.users){
-        if(u.Username==user){
-          this.userError='Username already taken!';
-          this.isValidUser=true;
-          break;
-        }else{
-         this.userError='Username is available!';
-         this.isValidUser=false;
-        }
-      }
-    }else if(user.length==0){
-          this.userError='';
-    }else
-    {
-      this.userError='username must be between 4-8 characters';
+   const combinedPattern = /^(?!.*\s)[\w-]+$/.test(user) //no spaces and 
+    if(!combinedPattern){
+      this.userError='Only allowed letters, number, -, and _';
       this.isValidUser=true;
     }
+    else{
+      if(user.length>3 && user.length<=12){
+        for(var u of this.users){
+          if(u.Username==user){
+            this.userError='Username already taken!';
+            this.isValidUser=true;
+            break;
+          }else{
+           this.userError='Username is available!';
+           this.isValidUser=false;
+          }
+        }
+      }else if(user.length==0){
+            this.userError='';
+      }else
+      {
+        this.userError='username must be between 4-12 characters';
+        this.isValidUser=true;
+      }
+    }
+    
   }
 
   checkPassword(e:any){
     let password = e.target.value; //user input password
 
     if (password.length>8 && password.length<=15){
-        this.passError='Password meeting accepting conditions';
+        this.passError='Password accepted!';
         this.isValidPass=false;
     }else if (password.length==0){      
       this.passError='';
     }else{
-      this.passError='password must be between 9-15 characters';
+      this.passError='Password must be between 9-15 characters!';
       this.isValidPass=true;
     }
   }
